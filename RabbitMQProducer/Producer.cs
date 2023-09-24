2a28946ee31be76285d1578f476e2890f2d0b0b0
Kaan Donmez <kndonmezz@gmail.com>
2023-09-24 15:24:34 +0300

    RabbitMQ example was added

---
+++ RabbitMQProducer/Producer.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using System.Timers;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "LoremIpsum",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+const string message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+
+var periodicTimer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+while (await periodicTimer.WaitForNextTickAsync())
+{
+    channel.BasicPublish(exchange: string.Empty,
+                         routingKey: "LoremIpsum",
+                         basicProperties: null,
+                         body: body);
+    Console.WriteLine($"Sent {message}");
+}
