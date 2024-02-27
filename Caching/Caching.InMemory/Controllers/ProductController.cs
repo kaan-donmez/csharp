47dfcd32a3743ce9b3dfc4f65bfd294efed9fc56
Kaan Donmez <kndonmezz@gmail.com>
2024-02-27 15:51:24 +0300

    Caching was uploaded

---
+++ Caching/Caching.InMemory/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Caching.InMemory
+{
+    public class ProductController : Controller
+    {
+
+        private IMemoryCache memoryCache;
+        public ProductController(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            memoryCache.Set("time", DateTime.Now.ToString());
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            ViewBag.time = memoryCache.Get("time");
+            return View();
+        }
+    }
+}
+
