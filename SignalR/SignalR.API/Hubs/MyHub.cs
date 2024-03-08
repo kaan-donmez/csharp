9c9e49cfff1ba0bfefb46f70b9748f5f9760b636
Kaan Donmez <kndonmezz@gmail.com>
2024-03-08 04:44:11 +0300

    SignalR was uploaded

---
+++ SignalR/SignalR.API/Hubs/MyHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.API;
+
+public class MyHub : Hub
+{
+    public static List<string> Names = new List<string>();
+    public async Task sendMessage(string name)
+    {
+        Names.Add(name);
+        await Clients.All.SendAsync("ReceiveNames", name);
+    }
+
+    public async Task getNames()
+    {
+        await Clients.All.SendAsync("ReceiveNames", Names);
+    }
+}
---
+++ SignalR/SignalR.API/Program.cs
@@ -0,0 +1,64 @@
+using SignalR.API;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:5152")
+                .AllowAnyHeader()
+                .WithMethods("GET", "POST")
+                .AllowCredentials();
+        });
+});
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.UseCors();
+app.MapHub<MyHub>("/MyHub");
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
